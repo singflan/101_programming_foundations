88c2d0d8caf53696a96ff4bfa43a366223e24726
Dustin Flanary <singflan@gmail.com>
2017-02-28 21:31:51 -0700

    more progress on calculator program

--- calculator.rb
+++ calculator.rb
@@ -1,27 +1,51 @@
-# ask the user for 2 numbers
-# ask the user for an operation to perform
-#perform the operation
-#output the result
-
-Kernel.puts("Welcome to the Calculator!")
-
-Kernel.puts("What's the first number? ")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What's the second number? ")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-elsif operator == '4'
-  result = number1.to_f() / number2.to_f()
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
+def valid_number?(num)
+  num.to_i != 0
+end
+
+prompt("Welcome to the Calculator!")
+
+number1 = ''
+loop do 
+  prompt("What's the first number? ")
+  number1 = Kernel.gets().chomp()
+
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number")
+  end
+end
+
+number2 = ''
+loop do 
+  prompt("What's the second number? ")
+  number2 = Kernel.gets().chomp()
+
+  if valid_number?(number2)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number")
+  end
+end
+
+
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+result = case operator
+  when '1'
+    number1.to_i() + number2.to_i()
+  when '2'
+    number1.to_i() - number2.to_i()
+  when '3'
+    number1.to_i() * number2.to_i()
+  when '4'
+    number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("")
 Kernel.puts("The result is #{result}")
