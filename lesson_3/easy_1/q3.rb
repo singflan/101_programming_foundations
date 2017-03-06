6c83904c8a58c837400ef53e69f99d7db4f1dc08
Dustin Flanary <singflan@gmail.com>
2017-03-06 14:05:49 -0700

    Finished question 3

---
+++ lesson_3/easy_1/q3.rb
@@ -0,0 +1,4 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+words = advice.split(" ")
+words[words.index('important')] = 'urgent'
+puts advice = words.join(' ')
