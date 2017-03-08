cf1d16d0e5a974a3d48d551e6d5aa79f54e5a0ed
Dustin Flanary <singflan@gmail.com>
2017-03-07 19:06:55 -0700

    more easy 2 exercises

---
+++ lesson_3/easy2_q5.rb
@@ -0,0 +1,5 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones.push('Dino')
+
+flintstones += ['Dino', 'Hoppy']
+p flintstones
---
+++ lesson_3/easy2_q8.rb
@@ -0,0 +1,5 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice2 = advice.slice!(39..-1)
+puts advice
+puts advice2
