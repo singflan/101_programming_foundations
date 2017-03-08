3c9e3786d13323a6af0be599b7a535ae9e1be803
Dustin Flanary <singflan@gmail.com>
2017-03-07 19:25:43 -0700

    finished Easy 2 Exercises

--- lesson_3/easy2_q8.rb
+++ lesson_3/easy2_q8.rb
@@ -1,5 +1,5 @@
 advice = "Few things in life are as important as house training your pet dinosaur."
 
-advice2 = advice.slice!(39..-1)
+advice2 = advice.slice!(0, advice.index('house'))
 puts advice
 puts advice2
