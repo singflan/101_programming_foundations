92aeb0c17fddd5d1cf0495e048780b9fd137124c
Dustin Flanary <singflan@gmail.com>
2017-03-07 18:29:43 -0700

    more easy 2 problems

--- lesson_3/easy2_q1.rb
+++ lesson_3/easy2_q1.rb
@@ -1,5 +1,7 @@
 ages = {"Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10}
 
 puts ages.key?('spot')
-puts ages.include('spot')
-puts ages.
+puts ages.include?('spot')
+
+additional_ages = {"Marilyn" => 22, "Spot" => 237}
+puts ages.merge!(additional_ages)
