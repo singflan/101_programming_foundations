6f3491d5170643060e543e8e3bd99b16887658f8
Dustin Flanary <singflan@gmail.com>
2017-03-17 23:43:23 -0600

    set up initialize method

---
+++ lesson_6/21.rb
@@ -0,0 +1,23 @@
+LIMIT_TO_WIN = 21
+DECK = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+deck = []
+def initalize_deck
+  unshuffled_deck = DECK * 4
+  shuffled_deck = unshuffled_deck.shuffle
+  p shuffled_deck
+end
+
+def deal_cards
+
+end
+
+def player_hit
+
+end
+
+def dealer_hit
+
+end
+
+initalize_deck
