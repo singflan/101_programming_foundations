5f3e158ff5b46382c9701e654beffaa6a84ab2d7
Dustin Flanary <singflan@gmail.com>
2017-03-24 15:07:57 -0600

    solved century problem

--- small_problems/easy_4/what_century_is_that.rb
+++ small_problems/easy_4/what_century_is_that.rb
@@ -1,23 +1,41 @@
 def century(year)
   end_string = ''
-  cent = (year - 1) / 100 == 0
-  case cent.chars.last
-  when 4, 5, 6, 7, 8, 9, 0
-    then end_string = 'th'
-  when 1
-    then end_string = 'st' unless year / 1000 == 1
+  cent = ((year - 1) / 100) + 1 
+  if year / 1000 == 1 || year / 1000 == 11
+    end_string = 'th'
+    return cent.to_s + end_string
+  end
 
-
-  cent = cent.to_s + end_string
+  case cent.to_s.chars.last
+  when '1'
+    end_string = 'st' unless year / 1000 == 1
+  when '2'
+    end_string = 'nd' unless year / 1000 == 1
+  when '3'
+    end_string = 'rd' unless year / 1000 == 1
+  else
+    end_string = 'th'
+  end
+    
+  cent_string = cent.to_s + end_string
+  cent_string
 end
-
+puts century(2000)
 puts century(2000) == '20th'
+puts century(2001)
 puts century(2001) == '21st'
+puts century(1965) 
 puts century(1965) == '20th'
+puts century(256)
 puts century(256) == '3rd'
+puts century(5)
 puts century(5) == '1st'
+puts century(10103)
 puts century(10103) == '102nd'
+puts century(1052)
 puts century(1052) == '11th'
+puts century(1127)
 puts century(1127) == '12th'
+puts century(11201)
 puts century(11201) == '113th'
 
