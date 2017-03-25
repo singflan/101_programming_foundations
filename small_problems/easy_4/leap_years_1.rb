43cd479d8e5e938b7a887002439120a77076d5bc
Dustin Flanary <singflan@gmail.com>
2017-03-25 15:51:31 -0600

    did leap year part 1

---
+++ small_problems/easy_4/leap_years_1.rb
@@ -0,0 +1,31 @@
+def leap_year?(year)
+  answer = nil
+  if year % 4 == 0
+    answer = true
+  else
+    answer = false
+  end
+
+  if year % 100 == 0
+   answer = false
+  end
+  
+  if year % 400 == 0
+    answer = true
+  end
+  answer
+end
+
+puts leap_year?(2016) == true
+puts leap_year?(2015) == false
+puts leap_year?(2100) == false
+puts leap_year?(2400) == true
+puts leap_year?(240000) == true
+puts leap_year?(240001) == false
+puts leap_year?(2000) == true
+puts leap_year?(1900) == false
+puts leap_year?(1752) == true
+puts leap_year?(1700) == false
+puts leap_year?(1) == false
+puts leap_year?(100) == false
+puts leap_year?(400) == true
