8388c7aa1053713268a06343de0ee344befe5d78
Dustin Flanary <singflan@gmail.com>
2017-04-21 14:36:35 -0600

    diamonds

--- small_problems/medium_1/diamonds.rb
+++ small_problems/medium_1/diamonds.rb
@@ -1,5 +1,26 @@
-def diamond(rows)
-
+def diamond(num)
+	if num == 1
+		puts "*"
+	elsif num > 1
+		num_of_spaces = (num - 1) / 2
+		num_of_stars = 1
+		loop do
+			space_string = " " * num_of_spaces
+			star_string = "*" * num_of_stars
+			puts "#{space_string}#{star_string}#{space_string}"
+			num_of_spaces -= 1
+			num_of_stars +=2
+			break if num_of_stars == num
+		end
+		loop do 
+			space_string = " " * num_of_spaces
+			star_string = "*" * num_of_stars
+			puts "#{space_string}#{star_string}#{space_string}"
+			break if num_of_stars == 1
+			num_of_spaces += 1
+			num_of_stars -=2
+		end
+	end
 end
 
 diamond(1)
