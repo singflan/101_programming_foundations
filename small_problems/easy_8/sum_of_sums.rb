52275b083d1bc15e7cc15d24e70856fa39313590
Dustin Flanary <singflan@gmail.com>
2017-04-25 12:50:35 -0600

    put in correct folder

--- small_problems/easy_8/sum_of_sums.rb
+++ small_problems/easy_8/sum_of_sums.rb
@@ -1,14 +1,8 @@
 def sum_of_sums(array)
-  sum = 0
-  index = array.size
-  array.each do |num|
-    sum = sum + (num * (index))
-    index -= 1
-  end
-  sum
+
 end
 
-puts sum_of_sums([3, 5, 2]) == 21
-puts sum_of_sums([1, 5, 7, 3]) == 36
+puts sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+puts sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
 puts sum_of_sums([4]) == 4
 puts sum_of_sums([1, 2, 3, 4, 5]) == 35
